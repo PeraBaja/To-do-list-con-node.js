cdd79d72cd37c061d141d30b916b2109ff0fd878
Benjamin Peralta <benjaminp158@gmail.com>
2024-05-12 14:08:45 -0300

    Inicializando proyecto

---
+++ main.mjs
@@ -0,0 +1,55 @@
+import { readFile } from 'node:fs/promises'
+import process, { exit } from 'node:process'
+import { deleteTask, getTasks, markTaskAsDone, newTask } from './todo.mjs'
+import { normalize } from 'node:path'
+
+let action = process.argv[2]
+let params = process.argv.slice(3)
+
+switch (action) {
+    case '--new': {
+        params = normalizeParams(params)
+        newTask(params[0], params[1], params[2])
+        break
+    }
+    case '--delete': {
+        validateId()
+        deleteTask(params[0])
+        break
+    }
+    case '--list': {
+        if (params[0] === undefined){
+            console.log(getTasks())
+        }
+        else {
+            console.log(getTasks(params[0]))
+        }
+        break
+    }
+    case '--done': {
+        validateId()
+        markTaskAsDone(params[0])
+        break
+    }
+    default: {
+        console.error(`Error. ${action} no es valido`)
+        break
+    }
+}
+
+function normalizeParams(params) {
+    params.map(param => {
+        if (param === undefined) {
+            return ''
+        }
+        return param
+    })
+    return params
+}
+
+function validateId(){
+    if (Number.isInteger(params[0])) {
+        console.error('Error id erronea')
+        exit(1)
+    }
+}
---
+++ todo.mjs
@@ -0,0 +1,81 @@
+import { readFileSync, writeFileSync, appendFileSync, appendFile } from 'node:fs'
+import { exit } from 'node:process'
+
+export function selectTask(id) {
+    let task
+    let tasks = getTasks()
+    task = tasks.find(task => {
+        let values = task.split(',')
+        return values[0] === id
+    })
+    if (task === undefined){
+        console.error(`La tarea no existe con el id ${id}`)
+        exit(1)
+    }
+
+    return task
+}
+
+
+export function deleteTask(id){
+    let task = selectTask(id)
+    let tasks = getTasks()
+    let i = tasks.indexOf(task)
+    if(i != -1){
+        tasks = tasks.splice(i, 1)
+    }
+
+    
+    console.log(`Tarea: ${task} eliminada`)
+    writeFileSync('./Recursos/To-do-list-db.csv', tasks.join('\n'),{
+        'encoding': 'utf8'
+    })
+}
+export function newTask(name, dueDate, label){
+    let tasks = getTasks()
+    let lastTaskId = tasks.pop().split(',')[0]
+    appendFileSync('./Recursos/To-do-list-db.csv',
+        `\n${Number(lastTaskId) + 1},${name},${dueDate},todo,${label}`
+    , {
+        'encoding': 'utf8'
+    })
+
+}
+
+export function markTaskAsDone(id){
+    let task = selectTask(id)
+    let values = task.split(',')
+    let tasks = getTasks()
+    let i = tasks.indexOf(task)
+    if (values[3] === 'todo'){
+        values[3] = 'done'
+    }
+    tasks[i] = values.join(',')
+    writeFileSync('./Recursos/To-do-list-db.csv', tasks.join('\n'),{
+        'encoding': 'utf8'
+    })
+}
+
+
+export function getTasks(filter = 'all'){
+    let filtersKeywords = ['todo', 'done']
+    let content = readFileSync('./Recursos/To-do-list-db.csv', {
+        'encoding': 'utf8'
+    })
+    let tasks = content.split('\n')
+    if (filter === 'all') {
+        return tasks
+    }
+    
+    if (filtersKeywords.includes(filter)) {
+        let filteredTasks = tasks.filter(task => {
+            let values = task.split(',')
+            
+            return values[3] === filter 
+        })
+
+        return filteredTasks
+    }
+
+    console.error(`Error. No se reconoce el filtro ${filter}`)
+}
