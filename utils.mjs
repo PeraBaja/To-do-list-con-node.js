d8bee585c8d35bea9a4955ae447f10919a8b4ce1
Benjamín Peralta <benjaminp158@gmail.com>
2024-08-23 13:28:12 -0300

    Corregido validateId no validaba

--- utils.mjs
+++ utils.mjs
@@ -4,7 +4,13 @@ export function normalizeParams(params) {
 }
 
 export function validateId(id){
-    if (Number.isInteger(id)) {
+    try 
+    {
+        id = Number(id)
+        if (!Number.isInteger(id)) throw new Error;
+    }
+    catch
+    {
         console.error('Error id erronea')
         process.exit(1)
     }
